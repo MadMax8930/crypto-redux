109f608084b9aca6fae2ca67c49f37b3ad88ad44
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-18 20:20:06 +0200

    styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,20 @@ const Navbar = () => {
             <Typography.Title level={2} className='logo'>
                <Link to='/'>Crypto Redux</Link>
             </Typography.Title>
-            {/* <Button className='menu-control-container'>
-
-            </Button> */}
+            <Menu theme='dark'>
+               <Menu.Item icon={<HomeOutlined />}>
+                  <Link to='/'>Home</Link>
+               </Menu.Item>
+               <Menu.Item icon={<FundOutlined />}>
+                  <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+               </Menu.Item>
+               <Menu.Item icon={<MoneyCollectOutlined />}>
+                  <Link to='/exchanges'>Exchanges</Link>
+               </Menu.Item>
+               <Menu.Item icon={<BulbOutlined />}>
+                  <Link to='/new'>News</Link>
+               </Menu.Item>
+            </Menu>
          </div>
       </div>
   )
