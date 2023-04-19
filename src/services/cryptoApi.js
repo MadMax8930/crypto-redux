96525021ebb5735e83c34e01aaee8b56b9c18227
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-19 20:33:03 +0200

    get coins

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+   'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+   reducerPath: 'cryptoApi',
+   baseQuery: fetchBaseQuery({ baseUrl }),
+   endpoints: (builder) => ({
+      getCryptos: builder.query({
+         query: () => createRequest('/coins')
+      })
+   })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
