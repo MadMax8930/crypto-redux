7b7003556c9884d31b4c380ed3e8dd8055d15dcc
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-20 01:41:28 +0200

    middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from '../services/cryptoApi';
+import { newsApi } from "../services/newsApi";
 
 export default configureStore({
    reducer: {
       [cryptoApi.reducerPath]: cryptoApi.reducer,
+      [newsApi.reducerPath]: newsApi.reducer,
    },
    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(cryptoApi.middleware),
-
+        getDefaultMiddleware().concat(cryptoApi.middleware).concat(newsApi.middleware)
 });
