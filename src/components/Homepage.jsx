c63310c36c271972cc5208eb92c4c94944bf82bc
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-19 23:09:37 +0200

    query count

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ import { Cryptocurrencies, News } from '../components';
 const { Title } = Typography;
 
 const Homepage = () => {
-   const { data, isFetching } = useGetCryptosQuery();
+   const { data, isFetching } = useGetCryptosQuery(10);
    const globalStats = data?.data?.stats;
 
    console.log(globalStats);
@@ -29,13 +29,12 @@ const Homepage = () => {
           <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
           <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
         </div>
-        <Cryptocurrencies simplified />
+        <Cryptocurrencies simplified={true} />
         <div className='home-heading-container'> 
           <Title level={2} className="home-title">Latest Crypto News</Title>
           <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
         </div>
-        <News simplified />
-
+        <News simplified={true} />
       </>
   )
 };
