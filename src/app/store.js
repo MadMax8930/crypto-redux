e5bea2a03df57bd99ab41cc0e59cf5f33786e406
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-19 21:10:42 +0200

    middleware update

--- src/app/store.js
+++ src/app/store.js
@@ -5,4 +5,7 @@ export default configureStore({
    reducer: {
       [cryptoApi.reducerPath]: cryptoApi.reducer,
    },
+   middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(cryptoApi.middleware),
+
 });
