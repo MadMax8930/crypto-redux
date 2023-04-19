22f8358dc06dba2a60b1b8636fdc8f2e6ee60b9c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-19 23:08:57 +0200

    cryptocurrencies page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
-const Cryptocurrencies = () => {
-   const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({ simplified }) => {
+   const count = simplified ? 10 : 100;
+   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+   const [searchTerm, setSearchTerm] = useState('');
 
    console.log(cryptos);
+   
+   useEffect(() => {
+      const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+      setCryptos(filteredData);
+   } ,[cryptosList, searchTerm])
+
+   if (isFetching) return 'Loading...';
 
   return (
      <>
+       {!simplified && (
+          <div className='search-crypto'>
+             <Input onChange={(e) => {setSearchTerm(e.target.value)}} placeholder='Search Cryptocurrency' />
+          </div>
+       )}
        <Row gutter={[32, 32]} className='crypto-card-container'>
           {cryptos?.map((currency) => (
             <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
@@ -24,8 +38,7 @@ const Cryptocurrencies = () => {
                   >
                      <p>Price: {millify(currency.price)}</p>
                      <p>Market Cap: {millify(currency.marketCap)}</p>
-                     <p>Daily Change: {millify(currency.change)}%</p>
-                     
+                     <p>Daily Change: {millify(currency.change)}%</p>       
                   </Card>
                </Link>
             </Col>
